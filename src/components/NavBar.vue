309dd9999a129f35ca51c3faa1bffd9eb42117f8
wathone <mg.wathonekhant@gmail.com>
2022-02-11 04:01:53 +0630

    adding css

---
+++ src/components/NavBar.vue
@@ -0,0 +1,31 @@
+<template>
+    <nav class="main-nav">
+        <router-link to="/">Project</router-link>
+        <router-link :to="{name:'AddProject'}">Add Project</router-link>
+    </nav>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+    .main-nav{
+        text-align: center;
+        margin: 40px auto;
+    }
+    .main-nav a{
+        display: inline-block;
+        text-decoration: none;
+        margin: 0 10px;
+        color: #999;
+        font-style: italic;
+        font-size: 18px;
+    }
+    a.router-link-active{
+        border-bottom: 2px solid aqua;
+        padding-bottom: 4px;
+    }
+</style>
