c951c5f90e5e71ca8c66cb7097872729221547da
wathone <mg.wathonekhant@gmail.com>
2022-02-11 16:58:24 +0630

    edit function

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/Home.vue'
 import AddProject from '../views/AddProject'
+import EditProject from '../views/EditProject'
 
 const routes = [
   {
@@ -12,6 +13,13 @@ const routes = [
     path: '/addproject',
     name: 'AddProject',
     component: AddProject
+  },
+  {
+    path: '/editproject/:id',
+    name: 'EditProject',
+    component: EditProject,
+    props:true
+
   },
 ]
 
---
+++ src/views/EditProject.vue
@@ -0,0 +1,39 @@
+<template>
+  <h1>Edit Project</h1>
+  <form @submit.prevent="AddProject">
+    <label>Project Title</label>
+    <input type="text" v-model="title">
+    <label>Project Detail</label>
+    <input type="text" v-model="detail">
+    <button>Add Project</button>
+  </form>
+</template>
+
+<script>
+export default {
+    props:["id"],
+    data(){
+        return{
+            title:"",
+            detail:""
+        }
+    },
+    mounted(){
+        fetch("http://localhost:3000/projects/"+this.id)
+        .then((response)=>{
+            return response.json()
+        })
+        .then((datas)=>{
+            this.title = datas.title,
+            this.detail = datas.detail
+        })
+        .catch(()=>{
+
+        })
+    }
+}
+</script>
+
+<style>
+
+</style>
